7b4d943192ac61c98bb7fa77354389bc14ab87c4
Bhagi <bhagyashreenalli@gmail.com>
2021-05-15 10:11:14 +0530

    Revert "git revert work"
    by mistakenly i have done the wrong code comiited
    
    This reverts commit 614a22d48e7a53964bdd2cf07dd9da4187029866.

--- 1.java
+++ 1.java
@@ -1,3 +1,1 @@
-Adding Idea1 in 1java file
-
-now i want ot test the git revert functionality
+Adding Idea 1 in 1.java file
--- 2.java
+++ 2.java
@@ -1,5 +1,3 @@
 HEllo world 
 adding idea1 into the 2java file
 Welcome to Git Course
-
-want to work on the git revert functinality
